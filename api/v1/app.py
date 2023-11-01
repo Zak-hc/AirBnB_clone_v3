f32c17407589ddb8893fd7fc044ecb0c36ebec93
Zak-hc <zhaci001@gmail.com>
2023-10-31 19:26:12 -0700

    printing by us

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#piw
+"""piw"""
 from os import environ
 from flask import Flask
 from models import storage
@@ -7,9 +7,12 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 # Register a function to be called when the app context is torn down
+
+
 @app.teardown_appcontext
 def close_storage():
     storage.close()
+
 
 if __name__ == "__main__":
     """Main Func"""
